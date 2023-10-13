88c0321a5dec384b1287c2ce0b1bf63291d93a02
Nandan1208 <87329688+Nandan1208@users.noreply.github.com>
2023-10-13 13:33:38 +0530

    Add files via upload

---
+++ fitpeo (1)/src/pages/AndroidLarge1.js
@@ -0,0 +1,218 @@
+import styles from "./AndroidLarge1.module.css";
+
+const AndroidLarge1 = () => {
+  return (
+    <div className={styles.androidLarge1}>
+      <div className={styles.sideMenu}>
+        <div className={styles.sideMenuChild} />
+        <img className={styles.sideMenuItem} alt="" src="/rectangle-32.svg" />
+        <div className={styles.listMenu}>
+          <img
+            className={styles.icon24OutlineKeySqua}
+            alt=""
+            src="/icon--24--outline--keysquare.svg"
+          />
+          <div className={styles.dashboard}>Dashboard</div>
+        </div>
+        <div className={styles.listMenu1}>
+          <img className={styles.dSquare1Icon} alt="" src="/3dsquare-1.svg" />
+          <div className={styles.product}>Product</div>
+          <div className={styles.chevronRight2} />
+        </div>
+        <div className={styles.listMenu2}>
+          <img
+            className={styles.userSquare1Icon}
+            alt=""
+            src="/usersquare-1.svg"
+          />
+          <div className={styles.customers}>Customers</div>
+        </div>
+        <div className={styles.listMenu3}>
+          <img
+            className={styles.dSquare1Icon}
+            alt=""
+            src="/walletmoney-2.svg"
+          />
+          <div className={styles.income}>Income</div>
+        </div>
+        <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+        <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+        <img className={styles.vectorIcon2} alt="" src="/vector1.svg" />
+        <div className={styles.listMenu4}>
+          <img
+            className={styles.messageQuestion1Icon}
+            alt=""
+            src="/messagequestion-1.svg"
+          />
+          <div className={styles.help}>Help</div>
+          <img
+            className={styles.chevronRight2Icon}
+            alt=""
+            src="/chevronright-2.svg"
+          />
+        </div>
+        <img className={styles.sideMenuInner} alt="" src="/ellipse-8@2x.png" />
+        <div className={styles.nandanN}>Nandan N</div>
+        <img
+          className={styles.chevronDown2Icon}
+          alt=""
+          src="/chevrondown-2.svg"
+        />
+        <div className={styles.dashboardParent}>
+          <div className={styles.dashboard1}>Dashboard</div>
+          <img className={styles.setting1Icon} alt="" src="/setting-1.svg" />
+        </div>
+        <img className={styles.ellipseIcon} alt="" src="/ellipse-9@2x.png" />
+        <div className={styles.nandanParent}>
+          <div className={styles.nandan}>Nandan</div>
+          <div className={styles.student}>Student</div>
+        </div>
+      </div>
+      <div className={styles.overview}>Overview</div>
+      <div className={styles.earning}>
+        <div className={styles.earningParent}>
+          <div className={styles.earning1}>Earning</div>
+          <div className={styles.k}>$1k</div>
+          <img className={styles.frameChild} alt="" src="/group-10.svg" />
+        </div>
+        <div className={styles.thisMonth}>
+          <b>3%</b>
+          <span className={styles.thisMonth1}> this month</span>
+        </div>
+      </div>
+      <div className={styles.earning2}>
+        <img className={styles.earningChild} alt="" src="/group-102.svg" />
+        <div className={styles.totalSales}>Total Sales</div>
+        <div className={styles.k1}>$8k</div>
+        <div className={styles.thisMonth2}>
+          <b>2%</b>
+          <span className={styles.thisMonth1}> this month</span>
+        </div>
+      </div>
+      <div className={styles.earning3}>
+        <img className={styles.earningItem} alt="" src="/group-101.svg" />
+        <div className={styles.k2}>$1K</div>
+        <div className={styles.thisWeek}>
+          <b>11%</b>
+          <span className={styles.thisMonth1}> this week</span>
+        </div>
+        <div className={styles.earning4}>Earning</div>
+      </div>
+      <div className={styles.search}>
+        <div className={styles.searchChild} />
+        <div className={styles.search1}>Search</div>
+        <img className={styles.search1Icon} alt="" src="/search-1.svg" />
+      </div>
+      <div className={styles.search2}>
+        <div className={styles.searchChild} />
+        <div className={styles.search1}>Search</div>
+        <img className={styles.search1Icon} alt="" src="/search-11.svg" />
+      </div>
+      <div className={styles.hiNandan}>Hi Nandan,</div>
+      <div className={styles.customers1}>
+        <img className={styles.customersChild} alt="" src="/rectangle-29.svg" />
+        <div className={styles.customers2}>Customers</div>
+        <div className={styles.customersThatBuy}>
+          Customers that buy products
+        </div>
+        <div className={styles.customersItem} />
+        <div className={styles.customersInner} />
+        <div className={styles.totalNewCustomersContainer}>
+          <p className={styles.p}>
+            <b>65%</b>
+          </p>
+          <p className={styles.totalNewCustomers}>Total New Customers</p>
+        </div>
+        <div className={styles.frameDiv} />
+      </div>
+      <div className={styles.monthlyEarning}>Monthly Earning</div>
+      <div className={styles.rectangleParent}>
+        <div className={styles.frameItem} />
+        <img
+          className={styles.chevronDown1Icon}
+          alt=""
+          src="/chevrondown-1.svg"
+        />
+        <div className={styles.quarterly}>Quarterly</div>
+      </div>
+      <div className={styles.product1}>
+        <img className={styles.productChild} alt="" src="/rectangle-30.svg" />
+        <div className={styles.productSell}>Product Sell</div>
+        <div className={styles.inStockParent}>
+          <div className={styles.inStock}>100 in stock</div>
+          <div className={styles.div}>$ 50</div>
+          <div className={styles.div1}>60</div>
+          <div className={styles.abstract3dParent}>
+            <div className={styles.abstract3d}>Abstract 3D</div>
+            <div className={styles.loremIpsumDolor}>
+              Lorem ipsum dolor elit.
+            </div>
+            <img
+              className={styles.unsplashwh2fhl0vEoIcon}
+              alt=""
+              src="/unsplashwh2fhl0v-eo@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.sarphensParent}>
+          <div className={styles.sarphens}>Sarphens</div>
+          <div className={styles.inStock1}>100 in stock</div>
+          <div className={styles.div2}>$ 50</div>
+          <div className={styles.div3}>60</div>
+          <div
+            className={styles.loremIpsumDolor1}
+          >{`Lorem ipsum dolor sit `}</div>
+          <img
+            className={styles.unsplasha2jNamzagIcon}
+            alt=""
+            src="/unsplasha2j-namzag@2x.png"
+          />
+        </div>
+        <div className={styles.frameParent}>
+          <div className={styles.productNameParent}>
+            <div className={styles.productName}>Product Name</div>
+            <div className={styles.stock}>Stock</div>
+            <div className={styles.price}>Price</div>
+            <div className={styles.totalSales1}>Total Sales</div>
+          </div>
+          <div className={styles.frameInner} />
+        </div>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.rectangleDiv} />
+          <img
+            className={styles.chevronDown1Icon1}
+            alt=""
+            src="/chevrondown-11.svg"
+          />
+          <div className={styles.last30Days}>Last 30 days</div>
+        </div>
+      </div>
+      <div className={styles.jan}>Jan</div>
+      <div className={styles.feb}>Feb</div>
+      <div className={styles.mar}>Mar</div>
+      <div className={styles.apr}>Apr</div>
+      <div className={styles.may}>May</div>
+      <div className={styles.jun}>Jun</div>
+      <div className={styles.jul}>Jul</div>
+      <b className={styles.aug}>Aug</b>
+      <div className={styles.sep}>Sep</div>
+      <div className={styles.oct}>Oct</div>
+      <div className={styles.nov}>Nov</div>
+      <div className={styles.dec}>Dec</div>
+      <div className={styles.androidLarge1Child} />
+      <div className={styles.androidLarge1Item} />
+      <div className={styles.androidLarge1Inner} />
+      <div className={styles.androidLarge1Child1} />
+      <div className={styles.androidLarge1Child2} />
+      <img className={styles.rectangleIcon} alt="" src="/rectangle-22.svg" />
+      <div className={styles.androidLarge1Child3} />
+      <div className={styles.androidLarge1Child4} />
+      <div className={styles.androidLarge1Child5} />
+      <div className={styles.androidLarge1Child6} />
+      <div className={styles.androidLarge1Child7} />
+      <div className={styles.androidLarge1Child8} />
+    </div>
+  );
+};
+
+export default AndroidLarge1;
